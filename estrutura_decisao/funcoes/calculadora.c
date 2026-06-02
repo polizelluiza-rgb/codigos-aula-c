41869e77082b3ee144682f0d043d4b310d9dca5a
LUIZA POLIZEL <polizelluiza@gmail.com>
2026-06-02 11:15:05 -0300

    funcoes

--- estrutura_decisao/funcoes/calculadora.c
+++ estrutura_decisao/funcoes/calculadora.c
@@ -1,9 +1,66 @@
-#include<stdio.h>
-#include<locale.h>
+#include <stdio.h>
+#include <locale.h>
 
-int main()
-{
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+void ola () {
+    printf("Seja bem-vindo a Calculadora!\n");
+}
+
+void Calcular (int opcao, float n1, float n2) {
+    float resultado = 0;
+
+    if (opcao == 1) {
+        resultado = n1 + n2;
+        printf("\na soma é: %.2f", resultado);
+    }
+      else if (opcao == 2) {
+        resultado = n1 - n2;
+        printf("\na subtração é: %.2f", resultado);
+    }
+       else if (opcao == 3) {
+        resultado = n1 * n2;
+        printf("\na multiplicação é: %.2f", resultado);
+    }  
+      else if (opcao == 4) {
+        if (n2 != 0) {
+            resultado = n1 / n2;
+            printf("\na divisão é: %.2f", resultado);
+
+            } else {
+            printf("\nnão é possível dividir por zero");
+          }
+        }
+    }
+
+
+int main(){
+   setlocale(LC_ALL, "pt_BR.UTF-8");
+   
+   ola();
+
+    int identificador = 0;
+    float n1 = 0;
+    float n2 = 0;
+
+    printf("\nescolha a operação que deseja");
+    printf("\n1 -> Soma");
+    printf("\n2 -> Subtração");
+    printf("\n3 -> Multiplicação");
+    printf("\n4 -> Divisão");
+    printf("\ndigite o número da opção:");
+    scanf("%d", &identificador);
+
+    if (identificador < 1 || identificador > 4) {
+        printf("\nopção inválida");
+        return 0;
+    }
+
+    printf("\ndigite o primeiro número:");
+    scanf("%f", &n1);
+
+    printf("\ndigite o segundo número:");
+    scanf("%f", &n2);
+
+    Calcular(identificador, n1, n2);
 
     return 0;
 }
--- estrutura_decisao/funcoes/pares.c
+++ estrutura_decisao/funcoes/pares.c
@@ -1,9 +1,31 @@
-#include<stdio.h>
-#include<locale.h>
+#include <stdio.h>
 
-int main()
-{
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+int main() {
+    int inicio = 0;
+    int fim = 0;
+    int i = 0;
+
+    printf("Digite o numero inicial: ");
+    scanf("%d", &inicio);
+
+    printf("Digite o numero final: ");
+    scanf("%d", &fim);
+
+    printf("\nAnalise dos numeros:\n");
+
+    
+    for (i = inicio; i <= fim; i++) {
+       
+        
+        if (i % 2 == 0) {
+            printf("%d é PAR\n", i);
+        }
+    
+        else {
+            printf("%d é ÍMPAR\n", i);
+        }
+       
+    }
 
     return 0;
 }
