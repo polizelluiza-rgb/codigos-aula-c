1bf0a32653b6743ccd62e0a287eacbfcab50b8d0
LUIZA POLIZEL <polizelluiza@gmail.com>
2026-06-02 16:38:47 -0300

    funcoes

--- estrutura_decisao/funcoes/academia.c
+++ estrutura_decisao/funcoes/academia.c
@@ -1,10 +1,34 @@
-#include<stdio.h>
-#include<locale.h>
+#include <stdio.h>
+#include <locale.h>
 
-int main()
-{
+void ola(){
+    printf("seja bem vindo!, escolha a academia de sua preferencia");
+}
+
+void academia() {
+    float VidaSaude = 12 * 39.99;
+    float SaudeEmDia = 12 * 48.99;
+    float FitnessPro = 12 * 58.99;
+
+    printf("\nVida Saude: R$ %.2f por ano", VidaSaude);
+    printf("\nSaude em Dia: R$ %.2f por ano", SaudeEmDia);
+    printf("\nFitness Pro: R$ %.2f por ano", FitnessPro);
+
+    if (VidaSaude < SaudeEmDia && VidaSaude < FitnessPro) {
+        printf("\na acacademia mais em conta e a Vida Saúde.");
+    }
+    printf("\nse voce quer treinar todos os dias, a academia Fitness Pro vale mais a pena");
+}
+
+
+
+
+int main() {
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
+    ola();
 
-    return 0;
+   academia();
+   
+   return 0;
 }
